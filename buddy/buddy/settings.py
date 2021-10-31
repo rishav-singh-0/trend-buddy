b0d7bda99246d61bd1c3be84222cdf6110c11329
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-10-31 20:11:00 +0530

    Moved django_secret_key to environment variable

--- buddy/buddy/settings.py
+++ buddy/buddy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+*!uu4^bjm1t1!gum2oo$)w*%z7rm20*wxbg^(fq)wyh!#ymrp'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
