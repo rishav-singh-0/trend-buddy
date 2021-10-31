b079ffc1bca62fd5b993eefe41565b3613a7ae1a
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-10-31 15:41:29 +0530

    Added Symbol and Candlestick models

--- buddy/data/models.py
+++ buddy/data/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Symbol(models.Model):
+    symbol = models.CharField(unique=True ,max_length=8)
+    
+    def __str__(self):
+        return self.symbol
+
+class Candle(models.Model):
+    symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+    time = models.IntegerField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    volume = models.FloatField()
+    
+    def __str__(self):
+        return f'{self.symbol}_{self.time}'
