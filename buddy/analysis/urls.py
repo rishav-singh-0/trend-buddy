d7c1d3d79ae7336c3958380d4d6e715b4dbe0cfb
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-11-01 19:51:18 +0530

    Adding analysis url

--- buddy/analysis/urls.py
+++ buddy/analysis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from analysis.views import ChartView, SymbolListView
+from analysis.views import ChartView, SymbolListView, AnalysisView
 
 urlpatterns = [
     path('chart/', SymbolListView.as_view(), name='chart'),
     path('chart/<symbol>', ChartView.as_view(), name='chart'),
+    path('analysis', AnalysisView.as_view(), name='analysis'),
 ]
