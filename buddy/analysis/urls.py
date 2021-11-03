98898272f1da81e8350dfdfd9fefd829e66efde2
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-11-04 01:17:13 +0530

    Added Dashboard

--- buddy/analysis/urls.py
+++ buddy/analysis/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from analysis.views import SymbolListView, AnalysisView
+from analysis.views import DashboardView, SymbolListView, AnalysisView
 
 urlpatterns = [
+    path('', DashboardView.as_view(), name='dashboard'),
     path('all-symbols/', SymbolListView.as_view(), name='all-symbols'),
     path('analysis/<symbol>', AnalysisView.as_view(), name='analysis'),
 ]
--- buddy/analysis/views.py
+++ buddy/analysis/views.py
@@ -4,16 +4,27 @@ from django.views import View
 from data.models import Symbol, Candle
 
 
+class DashboardView(View):
+    
+    def get(self, request, *args, **kwargs):
+        
+        favourite_symbols = []
+        for favourite in request.user.favourites.all(): # likes is the related name used in models
+            favourite_symbols.append(favourite.symbol_id)
+
+        return render(request, 'index.html', {'symbols': favourite_symbols})
+
+
 class SymbolListView(View):
 
     def get(self, request, *args, **kwargs):
 
         symbols = Symbol.objects.all()
-        favourite_symbol = []
+        favourite_symbols = []
 
         for favourite in request.user.favourites.all(): # likes is the related name used in models
-            favourite_symbol.append(favourite.symbol_id)
-        return render(request, 'analysis/all-symbols.html', {'symbols': symbols, 'favourites': favourite_symbol})
+            favourite_symbols.append(favourite.symbol_id)
+        return render(request, 'analysis/all-symbols.html', {'symbols': symbols, 'favourites': favourite_symbols})
 
 
 class AnalysisView(View):
