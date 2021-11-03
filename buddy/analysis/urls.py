ae2814ac9e6aa1c3d267655d542cda961caecb81
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-11-03 23:51:04 +0530

    Removed manual chart and added 'TradingView' chart

--- buddy/analysis/urls.py
+++ buddy/analysis/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from analysis.views import ChartView, SymbolListView, AnalysisView
+from analysis.views import SymbolListView, AnalysisView
 
 urlpatterns = [
     path('all-symbols/', SymbolListView.as_view(), name='all-symbols'),
-    path('chart/<symbol>', ChartView.as_view(), name='chart'),
-    path('analysis', AnalysisView.as_view(), name='analysis'),
+    path('analysis/<symbol>', AnalysisView.as_view(), name='analysis'),
 ]
--- buddy/analysis/views.py
+++ buddy/analysis/views.py
@@ -16,32 +16,8 @@ class SymbolListView(View):
         return render(request, 'analysis/all-symbols.html', {'symbols': symbols, 'favourites': favourite_symbol})
 
 
-class ChartView(View):
-
-    def get(self, request, symbol, *args, **kwargs):
-
-        processed_candlesticks = []
-        symbol = Symbol.objects.get(symbol=symbol)
-        candles = Candle.objects.filter(symbol=symbol)
-        
-        for data in candles:
-            candlestick = { 
-                'time': data.time, 
-                'open': data.open, 
-                'high': data.high, 
-                'low': data.low, 
-                'close': data.close 
-            }
-            processed_candlesticks.append(candlestick)
-
-        return render(request, 'analysis/chart.html', {'data': processed_candlesticks})
-
-
-class SelectionFormView():
-    pass
-
 class AnalysisView(View):
     
-    def get(self, request, *args, **kwargs):
+    def get(self, request, symbol, *args, **kwargs):
         
-        return render(request, 'analysis/analysis.html', {'data': ''})
+        return render(request, 'analysis/analysis.html', {'symbol': symbol})
