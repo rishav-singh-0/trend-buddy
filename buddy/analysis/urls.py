a7776614c7285f1c67e604178efa9371dd51b7b6
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-11-10 01:11:07 +0530

    Changed all-symbols url

--- buddy/analysis/urls.py
+++ buddy/analysis/urls.py
@@ -3,6 +3,6 @@ from analysis.views import DashboardView, SymbolListView, AnalysisView
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
-    path('all-symbols/', SymbolListView.as_view(), name='all-symbols'),
+    path('symbols/', SymbolListView.as_view(), name='all-symbols'),
     path('analysis/<symbol>', AnalysisView.as_view(), name='analysis'),
 ]
