b306bd734daef8655f2507cef5a89c0a89be6688
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-11-10 01:31:29 +0530

    Organized `Statergy` class

--- buddy/analysis/statergy.py
+++ buddy/analysis/statergy.py
@@ -2,21 +2,22 @@ import numpy as np
 import talib
 
 class Statergy():
-    def __init__(self):
+    def __init__(self, candles):
 
         # converting list of candles into indevidual arrays
         self.opens, self.highs, self.lows, self.closes = np.array([]), np.array([]), np.array([]), np.array([])
-        # closes = [ item.close for item in self.candles ]
-        # self.closes = np.array(closes)
         for candle in self.candles:
             np.append(self.opens, candle.open)
             np.append(self.highs, candle.high)
             np.append(self.lows, candle.low)
             np.append(self.closes, candle.close)
 
+        # closes = [ item.close for item in self.candles ]
+        # self.closes = np.array(closes)
+
 
     # Momentum Analysis
-    def rsi(self, candles, timeperiod, overbought, oversold):
+    def rsi(self, timeperiod, overbought, oversold):
         '''
         RSI - Relative Strength Index
 
@@ -30,8 +31,8 @@ class Statergy():
             and False if RSI is in oversold region
         '''
 
-        rsi_list = talib.RSI(self.closes, timeperiod=timeperiod)
-        latest_rsi = rsi_list[-1]
+        self.rsi_list = talib.RSI(self.closes, timeperiod=timeperiod)
+        latest_rsi = self.rsi_list[-1]
         print(f"The current rsi is {latest_rsi}")
 
         if latest_rsi > overbought:
@@ -42,7 +43,7 @@ class Statergy():
             return True
 
 
-    def macd(self, candles):
+    def macd(self):
         '''
         MACD - Moving Averages Convergence Divergence
 
@@ -57,7 +58,7 @@ class Statergy():
             and False if RSI is in oversold region
         '''
 
-        self.macd, self.macdsignal, self.macdhist = talib.MACD(self.closes, fastperiod=12, slowperiod=26, signalperiod=9)
+        self.macd_list, self.macdsignal, self.macdhist = talib.MACD(self.closes, fastperiod=12, slowperiod=26, signalperiod=9)
         print('macdhist:\n',self.macdhist)
         if self.macdhist[-1] > 0:
             return True
@@ -65,31 +66,30 @@ class Statergy():
 
 
     # Patterns
-
-    def morning_star(candle):
-        integer = talib.CDLMORNINGSTAR(candle.open, candle.high, candle.low, candle.close, penetration=0)
+    def morning_star(self):
+        integer = talib.CDLMORNINGSTAR(self.open, self.high, self.low, self.close, penetration=0)
         
-    def evening_star(candle):
-        integer = talib.CDLEVENINGSTAR(candle.open, candle.high, candle.low, candle.close, penetration=0)
+    def evening_star(self):
+        integer = talib.CDLEVENINGSTAR(self.open, self.high, self.low, self.close, penetration=0)
         
-    def engulfing(candle):
-        integer = talib.CDLENGULFING(candle.open, candle.high, candle.low, candle.close)
+    def engulfing(self):
+        integer = talib.CDLENGULFING(self.open, self.high, self.low, self.close)
 
-    def hammer(candle):
-        integer = talib.CDLHAMMER(candle.open, candle.high, candle.low, candle.close)
+    def hammer(self):
+        integer = talib.CDLHAMMER(self.open, self.high, self.low, self.close)
 
-    def hanging_man(candle):
-        integer = talib.CDLHANGINGMAN(candle.open, candle.high, candle.low, candle.close)
+    def hanging_man(self):
+        integer = talib.CDLHANGINGMAN(self.open, self.high, self.low, self.close)
 
-    def inverted_hammer(candle):
-        integer = talib.CDLINVERTEDHAMMER(candle.open, candle.high, candle.low, candle.close)
+    def inverted_hammer(self):
+        integer = talib.CDLINVERTEDHAMMER(self.open, self.high, self.low, self.close)
 
-    def ladder_bottom(candle):
-        integer = talib.CDLLADDERBOTTOM(candle.open, candle.high, candle.low, candle.close)
+    def ladder_bottom(self):
+        integer = talib.CDLLADDERBOTTOM(self.open, self.high, self.low, self.close)
 
-    def rickshaw_man(candle):
-        integer = talib.CDLRICKSHAWMAN(candle.open, candle.high, candle.low, candle.close)
+    def rickshaw_man(self):
+        integer = talib.CDLRICKSHAWMAN(self.open, self.high, self.low, self.close)
 
-    def shooting_star(candle):
-        integer = talib.CDLSHOOTINGSTAR(candle.open, candle.high, candle.low, candle.close)
+    def shooting_star(self):
+        integer = talib.CDLSHOOTINGSTAR(self.open, self.high, self.low, self.close)
 
