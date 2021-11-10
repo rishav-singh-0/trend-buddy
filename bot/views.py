7e2e3a0bc35a480efd01c5eaacc736e75284c306
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-11-10 17:32:46 +0530

    Updated views for the changed job

--- bot/views.py
+++ bot/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, HttpResponse
 from django.views import View
+import django_rq
 
-from .jobs import Bot
+from .bot import Bot
 
 class BotView(View):
     
-    def get(self, request, *args, **kwargs):
-        bot = Bot('BTCUSDT')
-        bot.start()
-        return HttpResponse("Printing on console")
+    def get(self, request, symbol, *args, **kwargs):
+        bot = Bot(symbol)
+        queue = django_rq.get_queue('day')
+        queue.create_job(bot.start)
+        return HttpResponse(f"Printing on console {symbol}")
         # return render(request, 'index.html', {'symbols': ''})
-
