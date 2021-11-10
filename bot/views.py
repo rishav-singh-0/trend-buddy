b5bacee1fba4eb6ff970650940bf673ef201b017
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-11-10 17:42:34 +0530

    Fixed enqueuing

--- bot/views.py
+++ bot/views.py
@@ -9,6 +9,6 @@ class BotView(View):
     def get(self, request, symbol, *args, **kwargs):
         bot = Bot(symbol)
         queue = django_rq.get_queue('day')
-        queue.create_job(bot.start)
+        queue.enqueue(bot.start)
         return HttpResponse(f"Printing on console {symbol}")
         # return render(request, 'index.html', {'symbols': ''})
