54219134dbc45894fec61bc06b987dff97481183
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-09-11 15:52:27 +0530

    updated data urls

--- data/urls.py
+++ data/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
-from .views import CandleView, NSEPopulateView, SymbolView, FavouriteView, CryotoPopulateView
+from django.urls import include, path
+
+from data.models import Exchange
+from .views import ExchangeView, CandleView, NSEPopulateView, SymbolView, FavouriteView, CryotoPopulateView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('exchange', ExchangeView, 'exchange')
+router.register('symbol', SymbolView, 'symbol')
+router.register('candle', CandleView, 'candle')
+router.register('favourite', FavouriteView, 'fovourite')
 
 urlpatterns = [
-    path('symbol/', SymbolView.as_view(), name='symbol'),
-    path('favourite/', FavouriteView.as_view(), name='favourite'),
-    path('candle/<symbol>/', CandleView.as_view(), name='candle'),
-    path('populate/crypto/', CryotoPopulateView.as_view(), name='populate'),
-    path('populate/nse/', NSEPopulateView.as_view(), name='populate'),
+    path('', include(router.urls)), 
+    path('populate/crypto/', CryotoPopulateView.as_view(), name='populate-crypto'),
+    path('populate/nse/', NSEPopulateView.as_view(), name='populate-nse'),
 ]
