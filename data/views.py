905fb6ee1de11dea6edf28cca27806b065cd8b40
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-09-11 15:51:52 +0530

    updated data views

--- data/views.py
+++ data/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from datetime import date, timedelta
 from django.views import View
 
-from data.models import Favourite, Symbol, Candle
+from rest_framework import viewsets, generics, permissions
+from data.serializers import ExchangeSerializers, FavouriteSerializers, SymbolSerializers, CandleSerializers
+
+from data.models import Exchange, Favourite, Symbol, Candle
 from data.populate import CryptoPopulate, NSEPopulate
 
 
@@ -20,16 +23,27 @@ class CryotoPopulateView(View):
 
 class NSEPopulateView(View):
     def get(self, request, *args, **kwargs):
-        stock = NSEPopulate('TCS', from_date='14-05-2020', to_date='14-05-2022')
-        result = stock.get_history_data()
+        stock = NSEPopulate('INFY', from_date='14-05-2020', to_date='14-05-2022')
+        stock.get_history_data()
         result = stock.save_candles()
         return HttpResponse(str(result))
 
 
-class SymbolView(View):
+class ExchangeView(viewsets.ModelViewSet):
+    '''
+    Taking data from db about all the Exchanges available
+    '''
+    queryset = Exchange.objects.all()
+    serializer_class = ExchangeSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
+class SymbolView(viewsets.ModelViewSet):
     '''
     Taking data from db about all the symbols available
     '''
+    queryset = Symbol.objects.all()
+    serializer_class = SymbolSerializers
+    permission_classes = [permissions.IsAuthenticated]
     
     def get(self, request, *args, **kwargs):
         symbols = Symbol.objects.all()
@@ -40,8 +54,14 @@ class SymbolView(View):
         return JsonResponse(processed_symbols, safe=False)
 
 
-class FavouriteView(View):
-    
+class FavouriteView(viewsets.ModelViewSet):
+    '''
+    Taking data from db about all the Exchanges available
+    '''
+    queryset = Favourite.objects.all()
+    serializer_class = FavouriteSerializers
+    permission_classes = [permissions.IsAuthenticated]
+
     def post(self, request, *args, **kwargs):
         symbol_id = request.POST['symbol_id']
         symbol = Symbol.objects.get(pk = symbol_id)
@@ -54,28 +74,34 @@ class FavouriteView(View):
         
         return JsonResponse({'favourite':favourite})
 
-class CandleView(View):
+class CandleView(viewsets.ModelViewSet):
     '''
     Taking data from db and converting it to candlesticks
     '''
+    queryset = Candle.objects.all()
+    serializer_class = CandleSerializers
+    # filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request, symbol, *args, **kwargs):
-
-        try:
-            symbol = Symbol.objects.get(symbol=symbol)
-            candles = Candle.objects.filter(symbol=symbol)
-        except Exception as e:
-            return JsonResponse({'error': str(e)})
+    # def get_queryset(self):
+    #     queryset = Candle.objects.all()
+    #     try:
+    #         symbol = Symbol.objects.get(symbol=symbol)
+    #         candles = Candle.objects.filter(symbol=symbol)
+    #     except Exception as e:
+    #         return JsonResponse({'error': str(e)})
         
-        processed_candlesticks = []
-        for data in candles:
-            candlestick = { 
-                'time': data.time, 
-                'open': data.open, 
-                'high': data.high, 
-                'low': data.low, 
-                'close': data.close 
-            }
-            processed_candlesticks.append(candlestick)
+    #     processed_candlesticks = []
+    #     for data in candles:
+    #         candlestick = { 
+    #             'time': data.time, 
+    #             'open': data.open, 
+    #             'high': data.high, 
+    #             'low': data.low, 
+    #             'close': data.close 
+    #         }
+    #         processed_candlesticks.append(candlestick)
             
-        return JsonResponse(processed_candlesticks, safe=False)
+    #     # return JsonResponse(processed_candlesticks, safe=False)
+    #     return queryset
+    
