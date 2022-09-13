422ba5cd6b06bb0ae5859bf165c9221934f27c89
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-09-13 22:41:13 +0530

    added 'create' function

--- data/serializers.py
+++ data/serializers.py
@@ -1,6 +1,9 @@
-import os
+import io
 from rest_framework import serializers
 from .models import Favourite, Exchange, Symbol, Candle
+from django.contrib.auth.models import User
+from .populate import CsvTradePopulate
+import pandas as pd
 
 
 class ExchangeSerializers(serializers.ModelSerializer):
@@ -34,3 +37,21 @@ class FavouriteSerializers(serializers.ModelSerializer):
     class Meta:
         model = Favourite
         fields = '__all__'
+    
+class FileUploadSerializer(serializers.Serializer):
+    file = serializers.FileField()
+
+    def create(self, data):
+        print(data,'\n\n')
+        # print(data['file'].read())
+        trade_data = CsvTradePopulate(User.objects.get(username='rishav'))
+        decoded_file = data['file'].read().decode()
+        io_string = io.StringIO(decoded_file)
+        trade_data.csv_data = pd.read_csv(io_string)
+        trade_data.format_zerodha()
+        trade_data.add_trade()
+        
+        return trade_data.save_trade()[0]
+        
+    class Meta:
+        fields = ('file',)
