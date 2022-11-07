10a536f52c7ca915ae64d23279e2cc4da9ec5542
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-11-07 18:49:57 +0530

    removed views from analysis app

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,8 +1,1 @@
 from django.urls import path
-from analysis.views import DashboardView, SymbolListView, AnalysisView
-
-urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),
-    path('symbols/', SymbolListView.as_view(), name='all-symbols'),
-    path('symbols/<symbol>', AnalysisView.as_view(), name='analysis'),
-]
