3c0b573a4f3e5a1e3c5d238f8ce75842b8af35f2
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2022-11-10 03:43:28 +0530

    added update_holding signal on each trade change

---
+++ bot/signals.py
@@ -0,0 +1,38 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import Holding, Trade
+
+
+@receiver(post_save, sender=Trade, dispatch_uid="update_stock_count")
+@receiver(post_delete, sender=Trade, dispatch_uid="update_stock_count")
+def update_holding(sender, instance, **kwargs):
+    symbol = instance.symbol_id
+    user = instance.user_id
+    trades = Trade.objects.filter(user_id=user,symbol_id=symbol)
+    quantity = 0
+    price = 0
+    for i in trades:
+        if i.trade_type=='BUY':
+            quantity+=i.quantity
+            price+=i.quantity*i.price
+        else:
+            quantity-=i.quantity
+            price-=i.quantity*i.price
+    if(quantity!=0):
+        price = price / quantity
+    else:
+        Holding.objects.filter(user_id=user, symbol_id=symbol).delete()
+        return
+    holding, created = Holding.objects.update_or_create(
+        user_id = user,
+        symbol_id = symbol,
+        defaults={
+            'quantity':quantity,
+            'buying_price':price,
+            'ltp':price,
+            'ltp_time':0
+        },
+    )
+    # print(holding, created)
+
+    
