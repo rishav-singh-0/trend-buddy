83573ad68bedce55180e68177dfdbbe515339588
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2023-08-25 00:22:51 +0530

    registered models

--- buddy/api/data/admin.py
+++ buddy/api/data/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Exchange, Symbol, Candle
 
-# Register your models here.
+admin.site.register(Exchange)
+admin.site.register(Symbol)
+admin.site.register(Candle)
