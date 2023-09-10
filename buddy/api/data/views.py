81df926fce7c63a1bbeffc580261a05b76c5fe48
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2023-09-10 10:28:42 +0530

    tested fetching api

--- buddy/api/data/views.py
+++ buddy/api/data/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from .get_data import YahooFinanceDataFetcher
 
 
 def index(request):
+
+    fetcher = YahooFinanceDataFetcher("TCS", "NSE")
+    fetcher.fetch_data("1-1-2020", "25-8-2023")
+    data = fetcher.save_to_database()
+    print(data)
+    
     return HttpResponse("Data Home Page")
